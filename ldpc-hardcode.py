1b4dfe7ca619b573820c3098090aa8b1509a97b4
Maxim_Susloparov <susloparov@wireless.iitp.ru>
2020-09-21 21:17:05 +0300

    decoder progress

--- ldpc-hardcode.py
+++ ldpc-hardcode.py
@@ -33,6 +33,29 @@ def CompareWordsPercent(iword, oword):
     return correctBits / float(N)
 
 
+def LDPCCheckingMatrix(n0, l, m, bl):
+    rows_H = m*bl
+    cols_H = n0*l
+    H = np.zeros(shape=(rows_H, cols_H))
+    # H0: first m rows
+    for i_row in range(m):
+        H[i_row][i_row*n0:(i_row+1)*n0] = 1.0
+    # the rest bl-1 blocks (random ones placement)
+    for i_block in np.arange(1, bl, 1):
+        for i_row in np.arange(i_block*m, (i_block+1)*m, 1):
+            H[i_row] = RandomizedSparseOnesVector(cols_H, n0)
+    return H
+
+
+# binary vector with randomly placed n_ones ones ('pi')
+def RandomizedSparseOnesVector(size, n_ones):
+    result = np.zeros(size)
+    result[0:n_ones] = 1
+    np.random.shuffle(result)
+    return result
+
+
+
 # Input word (transmitter)
 alphabet = [0, 1]
 #iword_debug = np.array( [alphabet[1], alphabet[0], alphabet[1], alphabet[1]] )
@@ -41,28 +64,39 @@ iword_release = RandomizedInputWord(alphabet, 16)
 iword = iword_release   # input word
 print(f"Sent word:\t{iword}")
 
+k_bits = iword.shape[0] # G: number of information bits
+n_cwl = k_bits + 100    # G: CodeWord Length
+n0 = 5  # H: ones per row
+l = 3   # H: one-stacks 
+m = 3   # H: rows in a block
+bl = 3  # H: H0 blocks
+# G = ? How...
+H = LDPCCheckingMatrix(n0, l, m, bl)
 
-# Coder
-# LDPC to be implemented
-coder_word = iword  # transmitter->coder
-print(f"Coded word:\t{coder_word}")
+
+# Encoder
+word_encoder = iword  # transmitter->encoder
+# I actually need to encode it. But more on that later...
+print(f"Encoded word:\t{word_encoder}")
 
 
 # Channel
-channelBitflipProb = 0.25  # probability of a bit to flip because of channel noise
-x = coder_word  # coder->channel
-y = BinarySymmetricalChannelWord(x, channelBitflipProb)
-print(f"Channel noise:\t{y} ({int(100-np.round(CompareWordsPercent(coder_word, y)*100))}% bitflip)")
+channelBitflipProb = 0.1  # probability of a bit to flip because of channel noise
+x_chan = word_encoder  # encoder->channel
+y_chan = BinarySymmetricalChannelWord(x_chan, channelBitflipProb)
+print(f"Channel noise:\t{y_chan} ({int(100-np.round(CompareWordsPercent(word_encoder, y_chan)*100))}% bitflip)")
 
 
 # Decoder
-# LDPC hard-decision decoder to be implemented
-decoded_word = y    # channel->decoder
-print(f"Decoded word:\t{y}")
+print(H)
+# c*H.T = 0 check
+# majority decoding thing
+word_decoded = y_chan    # channel->decoder
+print(f"Decoded word:\t{word_decoded}")
 
 
 # Receiver
-oword = decoded_word    # decoder->receiver
+oword = word_decoded    # decoder->receiver
 
 
 # Result comparison
