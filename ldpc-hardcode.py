1a3157d90053d22906628f3e0dc57aa211e6d306
Maxim_Susloparov <susloparov@wireless.iitp.ru>
2020-09-30 00:59:14 +0300

    progress

--- ldpc-hardcode.py
+++ ldpc-hardcode.py
@@ -39,39 +39,45 @@ def LDPCCheckingMatrix(n0, l, m, bl):
     H = np.zeros(shape=(rows_H, cols_H))
     # H0: first m rows
     for i_row in range(m):
-        H[i_row][i_row*n0:(i_row+1)*n0] = 1.0
+        H[i_row, i_row*n0:(i_row+1)*n0] = 1
+    H0 = H[0:m]
+    
     # the rest bl-1 blocks (random ones placement)
     for i_block in np.arange(1, bl, 1):
-        for i_row in np.arange(i_block*m, (i_block+1)*m, 1):
-            H[i_row] = RandomizedSparseOnesVector(cols_H, n0)
-    return H
+        H_bl = RandomizedColumnsInMatrix(H0)
+        H[i_block*m : (i_block+1)*m] = H_bl
+    return H.astype(int)
 
 
 # binary vector with randomly placed n_ones ones ('pi')
-def RandomizedSparseOnesVector(size, n_ones):
-    result = np.zeros(size)
-    result[0:n_ones] = 1.0
-    np.random.shuffle(result)
+def RandomizedColumnsInMatrix(matrix):
+    result = np.empty_like(matrix)
+    col_inds = np.arange(0, matrix.shape[1], 1)
+    np.random.shuffle(col_inds)
+    rowIterInd = 0
+    for randInd in col_inds:
+        result[:, rowIterInd] = matrix[:, randInd]
+        rowIterInd += 1
     return result
 
 
 def BitFlippingLDPCDecoding(word, H):
     result_word = np.copy(word)
-    isDecodingOver = False
     max_iterations = 100
     for i_iter in range(max_iterations):
         parityFailsRows = []    # indices of H rows with failed parity check
         for rowIndex, row in enumerate(H):
             bdp = BinaryDotProduct(row, result_word)
             if bdp != 0:
-                parityFailsRows.append(rowIndex)
+                parityFailsRows.append(rowIndex)  
         if len(parityFailsRows) == 0:
             break   # all parity checks are satisfied
         codewordParityFails = np.zeros_like(word)
-        for rowIndex in parityFailsRows:
+        allParityChecks = H.shape[0]
+        for rowIndex in enumerate(H):
             ones = np.where(H[rowIndex] == 1)[0] # Tanner graph lines (downwards)
             codewordParityFails[ones] += 1
-        bitFlipIndex = np.where(codewordParityFails == np.amax(codewordParityFails))[0][0]  # first max
+        bitFlipIndex = np.where(codewordParityFails > (allParityChecks/2))
         result_word[bitFlipIndex] = Bitflip(result_word[bitFlipIndex])
     return result_word
 
@@ -83,19 +89,19 @@ def BinaryDotProduct(row, word):
     return np.dot(row, word) % alphabetModule
 
 
-channelBitflipProb = 0.1  # probability of a bit to flip because of channel noise
+channelBitflipProb = 0.05  # probability of a bit to flip because of channel noise
 
 # Input word (transmitter)
-alphabet = [0.0, 1.0]
+alphabet = [0, 1]
 inputWordSize = 10
-#iword = np.full(inputWordSize, alphabet[0])   # debug input word
-iword = RandomizedInputWord(alphabet, inputWordSize)   # release input word
+iword = np.full(inputWordSize, alphabet[0])   # debug input word
+#iword = RandomizedInputWord(alphabet, inputWordSize)   # release input word
 print(f"Sent word:\t{iword}")
 
-n0 = 5  # H: ones per row
-l = 3   # H: one-stacks 
+n0 = 500  # H: ones per row
+l = 30   # H: one-stacks 
 m = 3   # H: rows in a block
-bl = 3  # H: H0 blocks
+bl = 7  # H: H0 blocks
 k_bits = iword.shape[0] # G: number of information bits
 n_cwl = n0*l    # G: CodeWord Length
 # G = ? How...
